78fdfaf8211e41bdd6a14e9b87f6ebe01a537212
Stefan Radulescu <41856723+MoRommy@users.noreply.github.com>
2022-04-06 20:10:19 +0300

    completed get_data()
    get_data() - read from keyboard the input data and stores it in a structure, then return a pointer to it.
    PS: I have to free the memory allocated here in the function from which I call it.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 #define command_length 15
 #define name_length 200
+#define header_size 5
 
 int add_last(void **arr, int *len, data_structure *data)
 {
@@ -29,20 +30,65 @@ int delete_at(void **arr, int *len, int index)
 	return 0;
 }
 
-// data_structure get_money(int type) {
-// 	data_structure data = (data_structure) malloc(sizeof(data_structure));
+int get_dim1(int type) {
+	if (type == 1)
+		return 1;
+	if (type == 2)
+		return 2;
+	return 4;
+}
 
-// }
+int get_dim2(int type) {
+	if (type == 1)
+		return 1;
+	return 4;
+}
 
-void insert(void **arr, int *len) {
-	int tipul_dedicatiei;
+data_structure *get_data() {
+	int type, b1, b2;
 	char *nume_s = (char *) calloc(name_length, 1);
 	char *nume_d = (char *) calloc(name_length, 1);
-	scanf("%d", &tipul_dedicatiei);
+	scanf("%d", &type);
 	scanf("%s", nume_s);
-	// data_structure bancnota_1 = get_money(tipul_dedicatiei);
-	// data_structure bancnota_2 = get_money(tipul_dedicatiei);
+	scanf("%d", &b1);
+	scanf("%d", &b2);
 	scanf("%s", nume_d);
+	int data_size = strlen(nume_s) + get_dim1(type) +
+				   strlen(nume_d) + get_dim2(type) + 2; //2 * \0
+	data_structure *data = (data_structure *) malloc(sizeof(data_structure));
+	head *h = (head *) malloc(sizeof(head));
+	h->type = type;
+	h->len = header_size + data_size;
+
+	void *arr = (void *)malloc(data_size);
+	int i = strlen(nume_s) + 1;
+	memcpy(arr, nume_s, i);
+	memcpy(arr + i, &b1, get_dim1(type));
+	i += get_dim1(type);
+	memcpy(arr + i, &b2, get_dim2(type));
+	i += get_dim2(type);
+	memcpy(arr + i, nume_d, strlen(nume_d) + 1);
+	i += strlen(nume_d) + 1;
+	
+	data->header = h;
+	data->data = arr;
+
+	free(nume_s);
+	free(nume_d);
+
+	// for (int k = 0; k < i; k++) {
+	// 	int c;
+	// 	memcpy(&c, arr + k, 1);
+	// 	printf("%u ", c);
+	// }
+	return data;
+}
+
+void insert(void **arr, int *len) {
+
+	data_structure *data = get_data();
+	add_last(arr, len, data);
+
 	// add_last(arr, len, &data)
 }
 
---
+++ test.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+	
+	void *arr = malloc(100);
+	char *s = (char *)malloc(50);
+	scanf("%s", s);
+	//printf("%s size: %d length: %ld\n", s, sizeof(s), strlen(s));
+
+	strcpy(arr, s);
+	unsigned int ds = 257;
+	memcpy(arr + strlen(s) + 1, &ds, 4);
+	unsigned char dd;
+	memcpy(&dd, arr + strlen(s) + 1, 4);
+	printf("%s %u\n", arr, dd);
+
+	for (int i = 0; i <= 15; i++) {
+		int c;
+		memcpy(&c, arr + i, 1);
+		printf("%u ", c);
+	}
+
+
+	free(s);
+	free(arr);
+	return 0;
+}
+
+
+/*
+
+
+8     ->   00001000
+54    ->   00110110
+250   ->   11111010
+5     ->   00000101
+
+
+100283912 -> 00000101 11111010 00110110 00001000
+
+
+
+232   ->   11101000
+253   ->   11111101
+
+65000 ->   11111101 11101000
+
+
+
+
+
+103   -> 01100111
+214   -> 11010110 
+18    -> 00010010
+0     -> 00000000
+
+1234535 ->   00000000 00010010 11010110 01100111
+
+*/
